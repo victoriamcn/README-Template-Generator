193c3b1084c610bb1d559f128a0828df3d9f5d6f
Victoria <vmcnorrill@gmail.com>
2023-03-21 21:37:33 -0400

    added .DS_Store to gitignore file

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please type a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are ways to use your application?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address.',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
