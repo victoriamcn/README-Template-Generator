feaf47d9deef21db4160bc998139650ad3f3893a
Victoria <vmcnorrill@gmail.com>
2023-03-24 12:53:02 -0400

    added code for index and generate files. license icon isn't rendering

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# README-Template-Generator
-A command-line application that dynamically generates a professional README.md file from a user's input
+
+
+![GitHub license](#license)
+
+
+# yes
+
+## Description
+
+yes
+
+## Table of Contents
+- [Installation](#installation)
+- [Tests](#tests)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Questions](#Questions)
+- 
+![GitHub license](#license)
+
+
+## Installation
+
+To install please run yes
+
+## Tests
+
+To test please run yes
+
+## Usage
+
+yes
+
+## Contributing
+
+yes
+
+## Questions
+
+If you have any questions or concerns, you may reach me at gmail
+
+
+    ## License
+    
+ This is licensed under APACHE 2.0
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n![GitHub license](#license)\n`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `
+    ## License
+    \n This is licensed under ${license}\n`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  //String literal for markup language
 
+  return `
+${renderLicenseLink(data.license)}
+
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Tests](#tests)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Questions](#Questions)
+- ${renderLicenseLink(data.license)}
+
+## Installation
+
+To install please run ${data.install}
+
+## Tests
+
+To test please run ${data.tests}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Questions
+
+If you have any questions or concerns, you may reach me at ${data.email}
+
+${renderLicenseSection(data.license)}
 `;
 }
 
